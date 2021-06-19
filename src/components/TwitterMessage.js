12ddf1fa538a9b69227f686f47fbd440b2e088bf
Daban Sadiq <dabansadiq@icloud.com>
2021-06-19 08:49:55 +0000

    Automatically backed up by Learn

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -8,21 +8,21 @@ class TwitterMessage extends React.Component {
       message: ''
     };
   }
-
-  handleChange = event => {
-    this.setState({
-      message: event.target.value
-    })
-  }
-
-  render() {
-    let charNumber = this.props.maxChars - this.state.message.length;
-    return (
-      <div>
-        <strong>Your message:</strong>
-        <input type="text" onChange={this.handleChange} value={this.state.message}/>
-        {charNumber}
-      </div>
+  //
+  // handleChange = event => {
+  //   this.setState({
+  //     message: event.target.value
+  //   })
+  // }
+  //
+  // render() {
+  //   let charNumber = this.props.maxChars - this.state.message.length;
+  //   return (
+  //     <div>
+  //       <strong>Your message:</strong>
+  //       <input type="text" onChange={this.handleChange} value={this.state.message}/>
+  //       {charNumber}
+  //     </div>
     );
   }
 }
